7383e6ea0e2c70d2148f452600193a7b5498491e
dudlves <dudlves17@gmail.com>
2025-07-11 22:51:15 -0300

    feat: implement main layout and structure of homepage

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+// Serve arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor da DevBurger rodando em localhost:${PORT}`);
+  console.log(`Servidor da DevBurger rodando em localhost:${PORT}`);
 });
